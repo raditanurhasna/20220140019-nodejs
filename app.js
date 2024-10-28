ffc43d18f536a572ba9efc82e5d510b9fcc44e3b
raditanurhasna <raditanurhasna1410@gmail.com>
2024-10-28 22:35:04 +0700

    CRUD_RESTAPI

--- app.js
+++ app.js
@@ -1,11 +1,19 @@
-const express = require('express');
+const express = require ('express');
 const app = express();
+const todoRoutes = require('./routes/todo.js');
 const port = 3000;
 
+app.use(express.json());
+
+app.use('/todos', todoRoutes);
+app.set('view engine','ejs');
 app.get('/', (req, res) => {
-    res.send('Hi, darling!');
+    res.render('index');
 });
 
+app.get('/contact',(req,res)=>{
+    res.render('contact');
+})
 app.listen(port, () => {
     console.log(`Server running at http://localhost:${port}/`);
 });
