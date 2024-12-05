398393deb3871ec0d5ce06194d1f52c14ed0543a
raditanurhasna <raditanurhasna1410@gmail.com>
2024-12-05 18:41:48 +0700

    STYLING_PAWC_019

--- app.js
+++ app.js
@@ -1,59 +1,64 @@
-const express = require('express');
+const express = require("express");
+const path = require("path");
 const app = express();
-const todoRoutes = require('./routes/tododb.js');
-require('dotenv').config();
+const todoRoutes = require("./routes/tododb.js");
+require("dotenv").config();
 const port = process.env.PORT;
-
-const db = require('./database/db');
-const expressLayouts = require('express-ejs-layouts')
-
-const session = require('express-session');
+const db = require("./database/db");
+const expressLayouts = require("express-ejs-layouts");
+const session = require("express-session");
 // Mengimpor middleware
-const authRoutes = require('./routes/authRoutes');
-const { isAuthenticated } = require('./middlewares/middleware.js');
+const authRoutes = require("./routes/authRoutes");
+const { isAuthenticated } = require("./middlewares/middleware.js");
 
 app.use(expressLayouts);
 app.use(express.json());
 
+app.use("/todos", todoRoutes);
+app.set("view engine", "ejs");
 
-app.use('/todos', todoRoutes)
-app.set('view engine','ejs')
+app.use(express.static(path.join(__dirname, "public")));
 
 app.use(express.urlencoded({ extended: true }));
 
 // Konfigurasi express-session
-app.use(session({
-    secret: process.env.SESSION_SECRET || 'your_secret_key', // Gunakan secret key yang aman
+app.use(
+  session({
+    secret: process.env.SESSION_SECRET || "your_secret_key", // Gunakan secret key yang aman
     resave: false,
     saveUninitialized: false,
-    cookie: { secure: false } // Set ke true jika menggunakan HTTPS
-}));
+    cookie: { secure: false }, // Set ke true jika menggunakan HTTPS
+  })
+);
 
-app.use('/', authRoutes);
+app.use("/", authRoutes);
 
-app.get('/', isAuthenticated, (req, res) => {
-    res.render('index', {
-        layout: 'layouts/main-layout'
-    });
+app.get("/", isAuthenticated, (req, res) => {
+  res.render("index", {
+    layout: "layouts/main-layout",
+  });
 });
 
-app.get('/contact',isAuthenticated, (req,res)=>{
-    res.render('contact', {
-        layout: 'layouts/main-layout'
-    });
+app.get("/contact", isAuthenticated, (req, res) => {
+  res.render("contact", {
+    layout: "layouts/main-layout",
+  });
 });
 
-app.get('/todo-view',isAuthenticated, (req, res) => {
-    db.query('SELECT * FROM todos', (err, todos) => {
-        if (err) return res.status(500).send('Internal Server Error');
-        res.render('todo', {
-            layout: 'layouts/main-layout',
-            todos: todos
-        });
+app.get("/todo-view", (req, res) => {
+  db.query("SELECT * FROM todos", (err, todos) => {
+    if (err) return res.status(500).send("Internal Server Error");
+    res.render("todo", {
+      layout: "layouts/main-layout",
+      todos: todos,
     });
+  });
+});
+
+app.use((req, res) => {
+  res.status(404).send("Page Not Found");
 });
 
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}/`);
+  console.log(`Server running at http://localhost:${port}/`);
 });
-
--- routes/todo.js
+++ routes/todo.js
@@ -1,48 +1,49 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
 
 let todos = [
   {
-    id: 1, task: "Belajar Node.js", completed: false
+    id: 1,
+    task: "Belajar Node.Js",
+    completed: false,
   },
   {
-    id: 2, task: "Membuat API", completed: false
+    id: 2,
+    task: "Membuat API",
+    completed: false,
   },
 ];
 
-//Endpoint untuk mendapatkan data
-router.get('/',(req, res) => {res.json(todos); });
+router.get("/", (req, res) => {
+  res.json(todos);
+});
 
-//Endpoint untuk menambahkan tugas
-router.post('/', (req, res) => {
+router.post("/", (req, res) => {
   const newTodo = {
     id: todos.length + 1,
     task: req.body.task,
-    completed: false
+    completed: false,
   };
   todos.push(newTodo);
   res.status(201).json(newTodo);
 });
 
-module.exports = router;
+router.delete("/:id", (req, res) => {
+  const todoIndex = todos.findIndex((t) => t.id === parseInt(req.params.id));
+  if (todoIndex === -1) return res.status(404).json({ message: "Tugas Tidak Ditemukan" });
 
-//Endpoint untuk menghapus tugas
-router.delete('/:id', (req, res) => {
-  const todoIndex = todos.findIndex(t => t.id === parseInt(req.params.id));
-  if (todoIndex === -1) return res.status(404).json({ message: 'Tugas tidak ditemukan' });
-
-  const deletedTodo = todos.splice(todoIndex, 1)[0]; //menghapus dan menyimpan todo yang dihapus
-  res.status(200).json({message:`Tugas'${deletedTodo.task}'telah dihapus`})
+  const deletedTodo = todos.splice(todoIndex, 1)[0];
+  res.status(200).json({ message: `Tugas ${deletedTodo.task} Telah Dihapus` });
 });
 
-//Endpoint untuk memperbarui tugas
-router.put('/:id', (req, res) => {
-  const todo = todos.find(t => t.id === parseInt(req.params.id));
-  if (!todo) return res.status(404).json({ message: 'Tugas tidak ditemukan' });
-  todo.task = req.body.task || todo.task;
+router.put("/:id", (req, res) => {
+  const todo = todos.find((t) => t.id === parseInt(req.params.id));
+  if (!todo) return res.status(404).json({ message: "Tugas Tidak Ditemukan" });
 
   res.status(200).json({
-    message: `Tugas dengan ID ${todo.id} telah diperbarui`,
-    updatedTodo: todo
-  })
-})
+    message: `Tugas Dengan ID ${todo.id} Telah Diperbarui`,
+    updatedTodo: todo,
+  });
+});
+
+module.exports = router;
